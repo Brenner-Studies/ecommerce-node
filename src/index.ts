8f94064048b28c25c615694a2c47bc3208d13477
Brenner Costa <costabrenner1@gmail.com>
2024-06-24 20:00:31 -0300

    Revert "First commit // initial project config"
    This reverts commit 4f74af485912a3b19eaa7edf70331a57967e13e7.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response} from "express";
-
-const app:Express = express();
-
-app.get('/', (req:Request, res:Response) => {
-    res.send('Servidor funcionando!')
-});
-
-app.listen(3000, () => { console.log('App Funcionando!')})
